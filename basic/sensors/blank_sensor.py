6f594a59e72577684f2dd9ba47128d54879e5a9d
Daisuke Yabuki <dxy@acm.org>
2009-05-23 19:31:58 +0900

    use single quote for consistency with other classes and add a brief class comment.

--- basic/sensors/blank_sensor.py
+++ basic/sensors/blank_sensor.py
@@ -3,7 +3,9 @@
 from sensor import *
 
 class BlankSensor(Sensor):
+  """A class for an analog input port with no sensor connected.
+  """
   def __init__(self):
     self.value = 0
-    self.product_name = ""
+    self.product_name = ''
 
